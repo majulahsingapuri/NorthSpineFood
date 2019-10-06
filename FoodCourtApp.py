fe4dc6dec60a71f50a79d4a12902cab30a3a6317
majulahsingapuri <bhsingapuri@gmail.com>
2019-10-06 23:38:24 +0800

    removed individual files, set min window size
    Individual files be readded at a later point of time. set min size to prevent window from getting too small

--- FoodCourtApp.py
+++ FoodCourtApp.py
@@ -9,8 +9,6 @@
 from stall import Stall
 from menu_item import Item
 from data import directory
-from main_menu import MainMenu
-from sub_menu import SubMenu
 import tkinter as tk
 import constants
 
@@ -43,6 +41,52 @@ class FoodCourtApp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+class MainMenu(tk.Frame):
+
+    # Parent is the previous frame in the hierarchy, controller is the reference to the app instance
+    def __init__(self, parent, controller, **kw):
+        tk.Frame.__init__(self, parent, **kw)
+
+        # Main Menu Title
+        main_title_label = tk.Label(self, text = "Main Menu", font = constants.LARGE_FONT)
+        main_title_label.place(relx = 0.25, rely = 0.0625, relheight = 0.125, relwidth = 0.5)
+
+        # Buttons Frame
+        buttons_frame = tk.Frame(self)
+        buttons_frame.place(relx = 0.125, rely = 0.375, relwidth = 0.75, relheight = 0.5)
+
+        # Select Stall Button
+        select_stall_button = tk.Button(buttons_frame, text = "Select Stall", font = constants.MEDIUM_FONT, command = lambda : self.select_stall_button_pressed())
+        select_stall_button.place(relx = 0.125, rely = 0.125, relheight = 0.25, relwidth = 0.75)
+
+        # Option 2 Button
+        option_two_button = tk.Button(buttons_frame, text = "Option 2", font = constants.MEDIUM_FONT, command = lambda : self.option_two_button_pressed())
+        option_two_button.place(relx = 0.125, rely = 0.625, relheight = 0.25, relwidth = 0.75)
+    
+    def select_stall_button_pressed(self):
+        # Changing view to SubMenu
+        app.show_frame(SubMenu)
+
+    def option_two_button_pressed(self):
+        # Whatever this button does
+        print("Option 2 pressed")
+
+class SubMenu(tk.Frame):
+
+    def __init__(self, parent, controller, **kw):
+        tk.Frame.__init__(self, parent, **kw)
+
+        # Setting title of the SubMenu
+        sub_title_label = tk.Label(self, text = "Sub Menu", font = constants.LARGE_FONT)
+        sub_title_label.place(relx = 0.2, rely = 0, relheight = 0.05, relwidth = 0.6)
+
+        back_button = tk.Button(self, text = "Back", command = lambda: self.back_button_pressed())
+        back_button.place(relx = 0.2, rely = 0.5, relheight = 0.3, relwidth = 0.5)
+
+    def back_button_pressed(self):
+        app.show_frame(MainMenu)
+
 # Running the app loop
 app = FoodCourtApp()
+app.minsize(200,200)
 app.mainloop()
