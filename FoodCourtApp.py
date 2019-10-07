b36c497d0c29a3b97c3a92d1d3fa413ce90de84f
majulahsingapuri <bhsingapuri@gmail.com>
2019-10-07 16:45:25 +0800

    Added Select Stall Page, About Us Page

--- FoodCourtApp.py
+++ FoodCourtApp.py
@@ -28,7 +28,7 @@ class FoodCourtApp(tk.Tk):
         self.frames = {}
 
         # Loading pages into memory
-        for F in (MainMenu, SubMenu):
+        for F in (MainMenu, SelectStall, AboutUs):
             frame = F(container, self)
             self.frames[F] = frame
             frame.place(relheight = 1, relwidth = 1)
@@ -60,28 +60,63 @@ class MainMenu(tk.Frame):
         select_stall_button.place(relx = 0.125, rely = 0.125, relheight = 0.25, relwidth = 0.75)
 
         # Option 2 Button
-        option_two_button = tk.Button(buttons_frame, text = "Option 2", font = constants.MEDIUM_FONT, command = lambda : self.option_two_button_pressed())
+        option_two_button = tk.Button(buttons_frame, text = "About Us :)", font = constants.MEDIUM_FONT, command = lambda : self.option_two_button_pressed())
         option_two_button.place(relx = 0.125, rely = 0.625, relheight = 0.25, relwidth = 0.75)
     
     def select_stall_button_pressed(self):
-        # Changing view to SubMenu
-        app.show_frame(SubMenu)
+        # Changing view to SelectStall
+        app.show_frame(SelectStall)
 
     def option_two_button_pressed(self):
-        # Whatever this button does
-        print("Option 2 pressed")
+        # Changing view to About Us
+        app.show_frame(AboutUs)
 
-class SubMenu(tk.Frame):
+class SelectStall(tk.Frame):
 
     def __init__(self, parent, controller, **kw):
         tk.Frame.__init__(self, parent, **kw)
 
-        # Setting title of the SubMenu
-        sub_title_label = tk.Label(self, text = "Sub Menu", font = constants.LARGE_FONT)
-        sub_title_label.place(relx = 0.2, rely = 0, relheight = 0.05, relwidth = 0.6)
+        # Setting title of the SelectStall
+        sub_title_label = tk.Label(self, text = "Select Stall", font = constants.LARGE_FONT)
+        sub_title_label.place(relx = 0.25, rely = 0.0625, relheight = 0.125, relwidth = 0.5)
 
+        # Back Button
         back_button = tk.Button(self, text = "Back", command = lambda: self.back_button_pressed())
-        back_button.place(relx = 0.2, rely = 0.5, relheight = 0.3, relwidth = 0.5)
+        back_button.place(relx = 0.0125, rely = 0.0625, height = 30, width = 50)
+
+        # Frame to hold all Stall Buttons
+        stall_buttons_frame = tk.Frame(self)
+        stall_buttons_frame.place(relx = 0.125, rely = 0.375, relwidth = 0.75, relheight = 0.5)
+
+        # Stall Buttons
+        counter = 0
+        for index, stall in enumerate(directory):
+            stall_button = tk.Button(stall_buttons_frame, text = stall.stall_name, command = lambda : self.stall_button_pressed(stall))
+            if index % 2 == 1:
+                stall_button.grid(column = 0, row = counter)
+            else:
+                stall_button.grid(column = 1, row = counter)
+                counter += 1
+
+
+    def back_button_pressed(self):
+        app.show_frame(MainMenu)
+
+    def stall_button_pressed(self, stall):
+        print("Chose stall " + stall.stall_name)
+
+class AboutUs(tk.Frame):
+
+    def __init__(self, parent, controller, **kw):
+        tk.Frame.__init__(self, parent, **kw)
+
+        # About Us Title
+        main_title_label = tk.Label(self, text = "About Us", font = constants.LARGE_FONT)
+        main_title_label.place(relx = 0.25, rely = 0.0625, relheight = 0.125, relwidth = 0.5)
+
+        # Back Button
+        back_button = tk.Button(self, text = "Back", command = lambda: self.back_button_pressed())
+        back_button.place(relx = 0.0125, rely = 0.0625, height = 30, width = 50)
 
     def back_button_pressed(self):
         app.show_frame(MainMenu)
